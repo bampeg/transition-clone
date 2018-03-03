690e3e7cd21f5f1b5f29cef20daa8383066d24cc
Courtenay <court.eccles@gmail.com>
2018-03-03 12:11:14 -0700

    Home page with Smuggler presentation finished

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,6 +3,29 @@ import './Home.css'
 import homepageRiderOwnedImg from '../../images/HomepageRiderOwnedForLife3.jpg'
 import FunFaces from '../../components/FunFaces/FunFaces'
 
+import carbonSmuggler1 from '../../images/HomePageFeature_CarbonSmuggler_1.jpg'
+import carbonSmuggler2 from '../../images/HomePageFeature_CarbonSmuggler_2.jpg'
+import carbonSmuggler3 from '../../images/HomePageFeature_CarbonSmuggler_3.jpg'
+import carbonSmuggler4 from '../../images/HomePageFeature_CarbonSmuggler_4.jpg'
+import carbonSmuggler5 from '../../images/HomePageFeature_CarbonSmuggler_5.jpg'
+import carbonSmuggler6 from '../../images/HomePageFeature_CarbonSmuggler_6.jpg'
+import carbonSmuggler7 from '../../images/HomePageFeature_CarbonSmuggler_7.jpg'
+import carbonSmuggler8 from '../../images/HomePageFeature_CarbonSmuggler_8.jpg'
+import carbonSmuggler9 from '../../images/HomePageFeature_CarbonSmuggler_9.jpg'
+import carbonSmuggler10 from '../../images/HomePageFeature_CarbonSmuggler_10.jpg'
+import carbonSmuggler11 from '../../images/HomePageFeature_CarbonSmuggler_11.jpg'
+import carbonSmuggler12 from '../../images/HomePageFeature_CarbonSmuggler_12.jpg'
+import carbonSmuggler13 from '../../images/HomePageFeature_CarbonSmuggler_13.jpg'
+import carbonSmuggler14 from '../../images/HomePageFeature_CarbonSmuggler_14.jpg'
+import carbonSmuggler15 from '../../images/HomePageFeature_CarbonSmuggler_15.jpg'
+import carbonSmuggler16 from '../../images/HomePageFeature_CarbonSmuggler_16.jpg'
+import carbonSmuggler17 from '../../images/HomePageFeature_CarbonSmuggler_17.jpg'
+import carbonSmuggler17B from '../../images/HomePageFeature_CarbonSmuggler_17B.jpg'
+import carbonSmuggler18 from '../../images/HomePageFeature_CarbonSmuggler_18.jpg'
+import carbonSmuggler19 from '../../images/HomePageFeature_CarbonSmuggler_19.jpg'
+
+
+
 
 export default function Home() {
         return (
@@ -10,7 +33,58 @@ export default function Home() {
                 <img className="Home_mainImg" src={homepageRiderOwnedImg} alt="" />
                 <FunFaces/>
                 <div className="Home_featurePres">FEATURE PRESENTATION</div>
-                <iframe className="Home_vimeo" title="FarmerPatrol" src="https://player.vimeo.com/video/258344386" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen/>
+                <iframe className="Home_vimeo" title="FarmerPatrol" src="https://player.vimeo.com/video/255323594" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen/>
+                <img className="Home_smugglerPres" src={carbonSmuggler1} alt=""/>
+                <iframe className="Home_vimeo" title="FarmerPatrol" src="https://player.vimeo.com/video/249437283" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen/>
+                <img className="Home_smugglerPres" src={carbonSmuggler2} alt=""/>
+                <img className="Home_smugglerPres" src={carbonSmuggler3} alt=""/>
+                <div className="Home_blockMessage">The Hunter.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler4} alt=""/>
+                <div className="Home_blockMessage">The Hunted.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler5} alt=""/>
+                <img className="Home_smugglerPres" src={carbonSmuggler6} alt=""/>
+                <div className="Home_blockMessage">The Smuggler loves to go fast.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler7} alt=""/>
+                <div className="Home_blockMessage">Fast enough to outrun a net.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler8} alt=""/>
+                <img className="Home_smugglerPres" src={carbonSmuggler9} alt=""/>
+                <img className="Home_smugglerPres" src={carbonSmuggler10} alt=""/>
+                <img className="Home_smugglerPres" src={carbonSmuggler11} alt=""/>
+                <div className="Home_blockMessage">The perfect mountain bike heist.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler12} alt=""/>
+                <div className="Home_blockMessage">Getaway grin.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler13} alt=""/>
+                <div className="Home_blockMessage">Production team.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler14} alt=""/>
+                <div className="Home_blockMessage">Cable cam? Nah.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler15} alt=""/>
+                <div className="Home_blockMessage">This one was legitimately scary. Timing was key.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler16} alt=""/>
+                <div className="Home_blockMessage">Introducing the long awaited Carbon Smuggler.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler17} alt=""/>
+                <div className="Home_blockMessage">Bone grey frame with the X01 build.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler17B} alt=""/>
+                <div className="Home_blockMessage">Gunsmoke blue frame with the X01 build.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler18} alt=""/>
+                <div className="Home_blockMessage">The details are worth a gander.</div>
+                <img className="Home_smugglerPres" src={carbonSmuggler19} alt=""/>
+                <div className="Home_blockMessage">Four years of work went into making the carbon Smuggler. Each bike is the product of many individuals here at Transition Bikes. We're pumped on the finished product, and hope you are as well!</div>
+                <div className="Home_megaFeatures">MORE MEGA FEATURES</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
         )
 }
