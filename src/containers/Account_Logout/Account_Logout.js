2122147d2842341ee46447f70ea6ec80d0c53c7a
Courtenay <court.eccles@gmail.com>
2018-03-05 09:33:52 -0700

    improved navbar styling

--- src/containers/Account_Logout/Account_Logout.js
+++ src/containers/Account_Logout/Account_Logout.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import './Account_Logout.css'
 
 export default class Account_Logout extends Component {
     render() {
         return(
-            <div>
+            <div className="Account_Logout_container">
                 <h1>You have successfully logged out.</h1>
-                <a href={ process.env.REACT_APP_LOGIN }><button>SIGN IN</button></a>
+                <a href={ process.env.REACT_APP_LOGIN }>SIGN IN</a>
             </div>
         )
     }
